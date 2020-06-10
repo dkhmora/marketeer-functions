aa02df668aa388ca4d53347238304dce262d4446
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-11 04:12:01 +0800

    Implemented order notifications for Merchants

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,39 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.sendOrderNotification = functions
+  .region("asia-northeast1")
+  .firestore.document("merchants/{merchantId}/orders/{orderId}")
+  .onCreate(async (snap, context) => {
+    const { merchantId } = context.params;
+
+    const orderData = snap.data();
+
+    let fcmTokens = [];
+
+    await admin
+      .firestore()
+      .collection("merchant_fcm")
+      .doc(merchantId)
+      .get()
+      .then((document) => {
+        return (fcmTokens = document.data().fcmTokens);
+      })
+      .catch((err) => console.log(err));
+
+    const orderNotifications = [];
+
+    fcmTokens.map((token) => {
+      orderNotifications.push({
+        notification: {
+          title: "New Order!",
+          body: `Order # ${orderData.orderNumber}; Total Amount: ${orderData.totalAmount}`,
+        },
+        token,
+      });
+    });
+
+    const response = await admin.messaging().sendAll(orderNotifications);
+    console.log(response);
+  });
