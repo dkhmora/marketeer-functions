0b41ecce4038bf2ab0702a2c69b8037610310140
darylmora <daryl_kiel_mora@yahoo.com>
2020-06-27 01:57:23 +0800

    Added order number for users

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const { firestore } = require("firebase-admin");
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -8,6 +9,8 @@ admin.initializeApp();
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const db = admin.firestore();
 
 exports.sendOrderNotification = functions
   .region("asia-northeast1")
@@ -19,8 +22,7 @@ exports.sendOrderNotification = functions
 
     let fcmTokens = [];
 
-    await admin
-      .firestore()
+    await db
       .collection("merchant_fcm")
       .doc(merchantId)
       .get()
@@ -43,4 +45,40 @@ exports.sendOrderNotification = functions
 
     const response = await admin.messaging().sendAll(orderNotifications);
     console.log(response);
+  });
+
+exports.keepUserOrderCount = functions
+  .region("asia-northeast1")
+  .firestore.document("/users/{userId}/orders/{orderId}")
+  .onCreate((snap, context) => {
+    return db.runTransaction(async (transaction) => {
+      // Get the metadata document and increment the count.
+      const { userId } = context.params;
+      const orderRef = snap.ref;
+      const orderNumberRef = db
+        .collection("users")
+        .doc(userId)
+        .collection("orders")
+        .doc("order_number");
+
+      const orderNumberData = await transaction
+        .get(orderNumberRef)
+        .then((document) => {
+          if (document.exists) {
+            return document.data();
+          } else {
+            transaction.set(orderNumberRef, { orderNumber: 0 });
+
+            return { orderNumber: 0 };
+          }
+        })
+        .catch((err) => console.error(err));
+
+      const number = orderNumberData.orderNumber + 1;
+
+      // Update the order document
+      transaction.update(orderRef, {
+        orderNumber: number,
+      });
+    });
   });
