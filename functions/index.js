be389d053f35779304c8004ff8425466a01f0d01
darylmora <daryl_kiel_mora@yahoo.com>
2020-07-07 04:20:21 +0800

    Added function to update merchant credits when an order status is marked as shipped

--- functions/index.js
+++ functions/index.js
@@ -15,52 +15,23 @@ const db = admin.firestore();
 exports.keepOrderCount_sendOrderNotificationToMerchant = functions
   .region("asia-northeast1")
   .firestore.document("/orders/{orderId}")
-  .onCreate((snap, context) => {
+  .onCreate(async (snap, context) => {
     return db
       .runTransaction(async (transaction) => {
         // Get the metadata document and increment the count.
         const orderData = snap.data();
-        const { userId, totalAmount, merchantId } = orderData;
+        const { userId } = orderData;
         const orderRef = snap.ref;
         const userRef = db.collection("users").doc(userId);
-        const merchantRef = db.collection("merchants").doc(merchantId);
-        let userData = null;
-        let merchantData = null;
 
-        await transaction.getAll(userRef, merchantRef).then((documents) => {
-          const userDoc = documents[0];
-          const merchantDoc = documents[1];
-
-          if (userDoc.exists) {
-            userData = userDoc.data();
+        const userData = await transaction.get(userRef).then((documents) => {
+          if (documents.exists) {
+            return documents.data();
           } else {
             console.error("Error: User does not exist!");
           }
 
-          if (merchantDoc.exists) {
-            merchantData = merchantDoc.data();
-          } else {
-            console.error("Error: Merchant does not exist!");
-          }
-
           return null;
-        });
-
-        // Calculate transaction fee and subtract it from current credit data
-        const { creditData } = merchantData;
-        const { credits, creditThreshold } = creditData;
-        const transactionFee = totalAmount * 0.05;
-        const newCredits = credits - transactionFee;
-        const newCreditThresholdReached =
-          newCredits < creditThreshold ? true : false;
-
-        console.log(transactionFee, newCredits, newCreditThresholdReached);
-
-        // Update merchant order number and credits fields
-        transaction.update(merchantRef, {
-          orderNumber: firestore.FieldValue.increment(1),
-          ["creditData.credits"]: newCredits,
-          ["creditData.creditThresholdReached"]: newCreditThresholdReached,
         });
 
         // Update user order number field
@@ -82,67 +53,42 @@ exports.keepOrderCount_sendOrderNotificationToMerchant = functions
           transactionFee,
         });
 
-        // Pass merchant order number and updated merchant credits for order notification
-        return {
-          incrementedMerchantOrderNumber,
-          newCredits,
-          newCreditThresholdReached,
-        };
+        // Pass merchant order number
+        return { incrementedMerchantOrderNumber };
       })
-      .then(
-        async ({
-          incrementedMerchantOrderNumber,
-          newCredits,
-          newCreditThresholdReached,
-        }) => {
-          // Send Order Notification to Merchant
-          const orderData = snap.data();
-          const { merchantId } = orderData;
-          let fcmTokens = [];
+      .then(async ({ incrementedMerchantOrderNumber }) => {
+        // Send Order Notification to Merchant
+        const orderData = snap.data();
+        const { merchantId } = orderData;
+        const merchantFcmRef = db.collection("merchant_fcm").doc(merchantId);
+        let fcmTokens = [];
 
-          await db
-            .collection("merchant_fcm")
-            .doc(merchantId)
-            .get()
-            .then((document) => {
-              if (document.exists) {
-                return (fcmTokens = document.data().fcmTokens);
-              }
-              return null;
-            })
-            .catch((err) => console.log(err));
-
-          const orderNotifications = [];
-
-          const warningMessage =
-            "Warning: Credit threshold reached. Please load up to receive orders.";
-
-          fcmTokens.map((token) => {
-            orderNotifications.push({
-              notification: {
-                title: "You've got a new order!",
-                body: `Order # ${incrementedMerchantOrderNumber}; Total Amount: ${orderData.totalAmount}; Credits left: ${newCredits}`,
-              },
-              token,
-            });
-
-            if (newCreditThresholdReached) {
-              orderNotifications.push({
-                notification: {
-                  title: "WARNING: You've run out of credit load!",
-                  body: `"Please load up in order to receive more orders.
-                    If you need assistance, please email us at support@marketeer.ph and we will help you load up."`,
-                },
-                token,
-              });
+        await merchantFcmRef
+          .get()
+          .then((document) => {
+            if (document.exists) {
+              return (fcmTokens = document.data().fcmTokens);
             }
-          });
+            return null;
+          })
+          .catch((err) => console.log(err));
 
-          return orderNotifications.length > 0
-            ? await admin.messaging().sendAll(orderNotifications)
-            : console.log(`No fcm token found for ${merchantId}`);
-        }
-      );
+        const orderNotifications = [];
+
+        fcmTokens.map((token) => {
+          orderNotifications.push({
+            notification: {
+              title: "You've got a new order!",
+              body: `Order # ${incrementedMerchantOrderNumber}; Total Amount: ${orderData.totalAmount}`,
+            },
+            token,
+          });
+        });
+
+        return orderNotifications.length > 0
+          ? await admin.messaging().sendAll(orderNotifications)
+          : console.log(`No fcm token found for ${merchantId}`);
+      });
   });
 
 exports.signInWithPhoneAndPassword = functions.https.onCall(
@@ -164,3 +110,68 @@ exports.signInWithPhoneAndPassword = functions.https.onCall(
     return { s: 200, t: token };
   }
 );
+
+exports.updateMerchantCredits = functions
+  .region("asia-northeast1")
+  .firestore.document("/orders/{orderId}")
+  .onUpdate(async (snap, context) => {
+    return db.runTransaction(async (transaction) => {
+      const orderData = snap.after.data();
+      const { totalAmount, merchantId, orderStatus } = orderData;
+      const merchantRef = db.collection("merchants").doc(merchantId);
+
+      const merchantData = await transaction
+        .get(merchantRef)
+        .then((document) => {
+          return document.data();
+        });
+
+      // Calculate transaction fee and subtract it from current credit data
+      const { creditData } = merchantData;
+      const { credits, creditThreshold } = creditData;
+      const transactionFee = totalAmount * 0.05;
+      const newCredits = credits - transactionFee;
+      const newCreditThresholdReached =
+        newCredits < creditThreshold ? true : false;
+
+      if (orderStatus.shipped.status) {
+        transaction.update(merchantRef, {
+          ["creditData.credits"]: newCredits,
+          ["creditData.creditThresholdReached"]: newCreditThresholdReached,
+        });
+      }
+
+      // Send notification to merchant if they have no more credits left
+      const merchantFcmRef = db.collection("merchant_fcm").doc(merchantId);
+      let fcmTokens = [];
+
+      await merchantFcmRef
+        .get()
+        .then((document) => {
+          if (document.exists) {
+            return (fcmTokens = document.data().fcmTokens);
+          }
+          return null;
+        })
+        .catch((err) => console.log(err));
+
+      const orderNotifications = [];
+
+      if (newCreditThresholdReached) {
+        fcmTokens.map((token) => {
+          orderNotifications.push({
+            notification: {
+              title: "WARNING: You've run out of credits!",
+              body: `"Please top up in order to receive more orders.
+                If you need assistance, please email us at support@marketeer.ph and we will help you load up."`,
+            },
+            token,
+          });
+        });
+      }
+
+      return orderNotifications.length > 0 && fcmTokens.length > 0
+        ? await admin.messaging().sendAll(orderNotifications)
+        : null;
+    });
+  });
